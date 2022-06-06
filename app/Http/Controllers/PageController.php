b36560e5a069a84e2880040e82be8a46deb30942
andrea1632 <andrea.bellucci10@libero.it>
2022-06-06 18:05:18 +0200

    added view welcome

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Festival;
 class PageController extends Controller
 {
-    //
+    public function index(){
+        $festivals = Festival::All();
+        return view('welcome', compact('festivals'));
+    }
 }
